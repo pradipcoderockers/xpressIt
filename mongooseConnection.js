e106e684e11c6963ffe4f2e71df426cbaa218565
pradipcoderockers <pradip.comat@gmail.com>
2018-11-28 17:06:55 +0530

    email templates

--- mongooseConnection.js
+++ mongooseConnection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var port = 8083;
-var dbPath = 'mongodb://localhost:27017/xpressIt';
+var dbPath = 'mongodb://localhost:27017/xpessit';
 var live = "sk_live_FDAGD0APJEBJhZTJSNIkXL80";
 var dev = "sk_test_7usOH1agjIcDtzlRyM64lhCy";
 var stripeKey = dev;
