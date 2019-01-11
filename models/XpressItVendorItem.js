b13b7196a3ea211d2e6e142e4878341c6856619c
pradipcoderockers <pradip.comat@gmail.com>
2019-01-11 20:08:39 +0530

    email templates

--- models/XpressItVendorItem.js
+++ models/XpressItVendorItem.js
@@ -1,14 +1,11 @@
 var connection = require('../mongooseConnection');
 var mongoose = connection.mongoose;
 var Schema = mongoose.Schema;
-
 //Schema Declarations
 var XpressItVendorItemSchema = Schema({
     name: { type: String, required: true, trim: true },
     price: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
-    categoryId: { type: String, required: true, trim: true },
-    subCategoryId: { type: String, required: true, trim: true },
     imageLink: { type: String, required: true, trim: true, default: 'imageLink' },
     deleted: { type: Boolean, required: true, trim: true, default: false },
     vendorId: { type: String, ref: 'XpressItUser' },
@@ -18,9 +15,6 @@ var XpressItVendorItemSchema = Schema({
         timestamps: true,
         collection: "XpressItVendorItem"
     });
-
-
 //Model Declarations
 var XpressItVendorItem = mongoose.model('XpressItVendorItem', XpressItVendorItemSchema);
-
 module.exports = XpressItVendorItem;
